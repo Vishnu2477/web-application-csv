6d415cf9b606b5568bb22809da476ed1ec3d5e4b
Vishnu2477 <82508425+Vishnu2477@users.noreply.github.com>
2021-05-25 11:38:42 +0530

    Add files via upload

---
+++ export fuctions.php
@@ -0,0 +1,14 @@
+if(isset($_POST["Export"])){
+     
+      header('Content-Type: text/csv; charset=utf-8');  
+      header('Content-Disposition: attachment; filename=data.csv');  
+      $output = fopen("php://output", "w");  
+      fputcsv($output, array('ID', 'First Name', 'Last Name', 'Email', 'Joining Date'));  
+      $query = "SELECT * from employeeinfo ORDER BY emp_id DESC";  
+      $result = mysqli_query($con, $query);  
+      while($row = mysqli_fetch_assoc($result))  
+      {  
+           fputcsv($output, $row);  
+      }  
+      fclose($output);  
+ }  
